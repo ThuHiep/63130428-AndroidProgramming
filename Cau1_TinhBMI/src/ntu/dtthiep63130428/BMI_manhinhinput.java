8a2704034bb0eb9d35b2f186ac94d756d891e72c
ThuHiep <145564911+ThuHiep@users.noreply.github.com>
2024-03-24 17:03:30 +0700

    tạo bố cục cho màn hình input

--- Cau1_TinhBMI/src/ntu/dtthiep63130428/BMI_manhinhinput.java
+++ Cau1_TinhBMI/src/ntu/dtthiep63130428/BMI_manhinhinput.java
@@ -1,12 +1,22 @@
 package ntu.dtthiep63130428;
 
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.JButton;
+import javax.swing.JComboBox;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+import java.awt.Font;
 
 public class BMI_manhinhinput {
 
 	private JFrame frame;
+	    
 
 	/**
 	 * Launch the application.
@@ -36,8 +46,44 @@ public class BMI_manhinhinput {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 649, 490);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-	}
+		frame.getContentPane().setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("BMI CACULATE");
+		lblNewLabel.setFont(new Font("#9Slide01 Tieu de ngan", Font.ITALIC, 30));
+		lblNewLabel.setBounds(193, 10, 268, 74);
+		frame.getContentPane().add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Male");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel_1.setBounds(95, 197, 81, 33);
+		frame.getContentPane().add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Female");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel_2.setBounds(450, 202, 71, 23);
+		frame.getContentPane().add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Enter your weight:");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel_3.setBounds(150, 254, 172, 33);
+		frame.getContentPane().add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("Enter your height:");
+		lblNewLabel_4.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblNewLabel_4.setBounds(150, 307, 172, 23);
+		frame.getContentPane().add(lblNewLabel_4);
+		
+		JButton btnNewButton = new JButton("CHECK");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 24));
+		btnNewButton.setBounds(255, 354, 142, 67);
+		frame.getContentPane().add(btnNewButton);
 
+   
+        
+        // Components initialization
+        String[] weightUnits = {"Kilogram (kg)", "Pound (lb)"};
+        String[] pressureUnits = {"Pascal (Pa)", "Psi (psi)"};
+	}
 }
--- Cau1_TinhBMI/src/ntu/dtthiep63130428/BMI_manhinhketqua.java
+++ Cau1_TinhBMI/src/ntu/dtthiep63130428/BMI_manhinhketqua.java
@@ -32,11 +32,12 @@ public class BMI_manhinhketqua extends JFrame {
 	 */
 	public BMI_manhinhketqua() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 663, 519);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
 	}
 
 }
