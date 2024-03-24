18c4894fc766ab8acefe1a22348b8d385c79e343
ThuHiep <145564911+ThuHiep@users.noreply.github.com>
2024-03-24 13:25:48 +0700

    Khởi tạo câu 2 bài thi giữa kì

---
+++ Cau2_WeatherApp/app/src/main/java/ntu/dtthiep63130428/cau2_weatherapp/MainActivity.java
@@ -0,0 +1,14 @@
+package ntu.dtthiep63130428.cau2_weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
