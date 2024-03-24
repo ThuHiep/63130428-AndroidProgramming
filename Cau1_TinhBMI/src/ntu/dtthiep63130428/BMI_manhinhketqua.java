98f0c8117466f46cd936ef4c2fa8b7008c012e1c
ThuHiep <145564911+ThuHiep@users.noreply.github.com>
2024-03-24 22:45:28 +0700

    Hoàn thành câu 1

--- Cau1_TinhBMI/src/ntu/dtthiep63130428/BMI_manhinhketqua.java
+++ Cau1_TinhBMI/src/ntu/dtthiep63130428/BMI_manhinhketqua.java
@@ -17,7 +17,8 @@ public class BMI_manhinhketqua extends JFrame {
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	private JTextField txtbmi;
-	private JTextField textField_1;
+	private JTextField txtkhuyen;
+	private JTextField txtthongbao;
 
 	/**
 	 * Launch the application.
@@ -49,9 +50,9 @@ public class BMI_manhinhketqua extends JFrame {
 		contentPane.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("YOUR BMI");
-		lblNewLabel.setForeground(new Color(73, 37, 37));
-		lblNewLabel.setFont(new Font("#9Slide01 Noi dung ngan", Font.PLAIN, 30));
-		lblNewLabel.setBounds(307, 26, 167, 58);
+		lblNewLabel.setForeground(new Color(192, 121, 200));
+		lblNewLabel.setFont(new Font("#9Slide01 Noi dung ngan", Font.PLAIN, 40));
+		lblNewLabel.setBounds(273, 22, 226, 55);
 		contentPane.add(lblNewLabel);
 		
 		JLabel lblNewLabel_1 = new JLabel("");
@@ -64,20 +65,52 @@ public class BMI_manhinhketqua extends JFrame {
 		txtbmi.setText(String.format("%.2f", bmi));
 		txtbmi.setEditable(false);
 		txtbmi.setFont(new Font("#9Slide01 Tieu de ngan", Font.PLAIN, 40));
-		txtbmi.setBounds(127, 142, 198, 103);
+		txtbmi.setBounds(43, 118, 118, 113);
 		contentPane.add(txtbmi);
 		txtbmi.setColumns(10);
 		
-		JLabel lblNewLabel_2 = new JLabel("Your advice:");
-		lblNewLabel_2.setFont(new Font("#9Slide01 Noi dung ngan", Font.PLAIN, 20));
-		lblNewLabel_2.setBounds(10, 331, 125, 41);
-		contentPane.add(lblNewLabel_2);
+		txtkhuyen = new JTextField();
+		txtkhuyen.setFont(new Font("#9Slide02 Noi dung dai", Font.PLAIN, 18));
+		txtkhuyen.setBounds(171, 332, 301, 65);
+		contentPane.add(txtkhuyen);
+		txtkhuyen.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setFont(new Font("#9Slide02 Noi dung dai", Font.PLAIN, 20));
-		textField_1.setBounds(163, 322, 297, 68);
-		contentPane.add(textField_1);
-		textField_1.setColumns(10);
+		txtthongbao = new JTextField();
+		txtthongbao.setFont(new Font("#9Slide01 Noi dung ngan", Font.BOLD, 30));
+		if (bmi < 18.5) {
+            txtthongbao.setText("Thiếu cân");
+            txtkhuyen.setText("Hãy tăng cân một cách lành mạnh.");
+            
+        } else if (bmi >= 18.5 && bmi < 24.9) {
+        	 txtthongbao.setText("Cân đối");
+        	 txtkhuyen.setText("Hãy duy trì chế độ ăn uống và luyện tập thể dục.");
+        } else if (bmi >= 25 && bmi < 29.9) {
+        	 txtthongbao.setText("Thừa cân");
+        	 txtkhuyen.setText("Hãy giảm cân một cách cẩn thận và lành mạnh.");
+        } else if (bmi >= 30 && bmi < 34.9) {
+        	 txtthongbao.setText("Béo phì");
+  
+        	 txtkhuyen.setText(" Hãy tập thể dục thường xuyên và ăn uống cân đối.");
+        } else {
+        	 txtthongbao.setText("Béo phì nguy hiểm");
+        
+        	 txtkhuyen.setText("Hãy tìm kiếm sự giúp đỡ y tế ngay lập tức.");
+        }
+		txtthongbao.setBounds(295, 149, 177, 65);
+		contentPane.add(txtthongbao);
+		txtthongbao.setColumns(10);
+		
+		JLabel lb3 = new JLabel("");
+		Image muiten = new ImageIcon(this.getClass().getResource("/muiten.jpg")).getImage();
+		lb3.setIcon(new ImageIcon(muiten));
+		lb3.setBounds(171, 159, 118, 55);
+		contentPane.add(lb3);
+		
+		JLabel lbthu = new JLabel("");
+		Image thu = new ImageIcon(this.getClass().getResource("/thu.jpg")).getImage();
+		lbthu.setIcon(new ImageIcon(thu));
+		lbthu.setBounds(43, 284, 105, 113);
+		contentPane.add(lbthu);
 		
 		
 		
