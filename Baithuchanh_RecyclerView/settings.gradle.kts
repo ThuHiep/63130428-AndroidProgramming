2ff5d4f954eb7bff3a84775b344117a7e0b17083
ThuHiep <145564911+ThuHiep@users.noreply.github.com>
2024-04-16 22:06:23 +0700

    bài thực hành về RecyclerView

---
+++ Baithuchanh_RecyclerView/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Baithuchanh_RecyclerView"
+include(":app")
+ 
