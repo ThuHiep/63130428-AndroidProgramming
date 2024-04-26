234e52dcab11c75401e15b72f8ae4290618e1eb4
ThuHiep <145564911+ThuHiep@users.noreply.github.com>
2024-04-26 11:03:01 +0700

    create fragment

--- BottomNavigation/app/src/main/java/ntu/dtthiep/bottomnavigation/MainActivity.java
+++ BottomNavigation/app/src/main/java/ntu/dtthiep/bottomnavigation/MainActivity.java
@@ -3,12 +3,21 @@ package ntu.dtthiep.bottomnavigation;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+    private BottomNavigationView bottomNavigationView;
+    private FrameLayout frameLayout;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationView = findViewById(R.id.bottomNavigation);
+        frameLayout = findViewById(R.id.framelayout);
+
     }
 }
