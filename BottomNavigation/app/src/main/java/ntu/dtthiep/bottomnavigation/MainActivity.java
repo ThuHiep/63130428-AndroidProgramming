93d22094b084a3b9e92f610bf014cd7827aa4417
ThuHiep <145564911+ThuHiep@users.noreply.github.com>
2024-04-26 11:17:13 +0700

    select listener

--- BottomNavigation/app/src/main/java/ntu/dtthiep/bottomnavigation/MainActivity.java
+++ BottomNavigation/app/src/main/java/ntu/dtthiep/bottomnavigation/MainActivity.java
@@ -1,8 +1,13 @@
 package ntu.dtthiep.bottomnavigation;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.widget.FrameLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -19,5 +24,37 @@ public class MainActivity extends AppCompatActivity {
         bottomNavigationView = findViewById(R.id.bottomNavigation);
         frameLayout = findViewById(R.id.framelayout);
 
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+
+                if(itemId==R.id.navHome){
+                    LoadFragment(new HomeFragment());
+
+                    
+                } else if (itemId==R.id.navSearch) {
+                    LoadFragment(new searchfragment());
+                    
+                } else if (itemId==R.id.navNot) {
+                    LoadFragment(new notififragment());
+                    
+                }else {
+                    LoadFragment(new profilefragment());
+
+                }
+
+                return false;
+            }
+        });
+
+    }
+    private void LoadFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        fragmentTransaction.add(R.id.framelayout, fragment);
+        fragmentTransaction.commit();
     }
 }
